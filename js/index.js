a653f41cc76ac3476e69c026a8c8769e36e1ba83
Ivan <ivan.baena04@gmail.com>
2015-12-04 06:26:30 -0500

    edited about-content transition to 4 columns

--- js/index.js
+++ js/index.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
     $('.parallax').parallax();
 
-    
+      var options = [
+    {selector: '#image-test', offset: 50, callback: 'Materialize.toast("Close!", 1500 )' },
+    {selector: '#image-test', offset: 205, callback: 'Materialize.toast("Closer!", 1500 )' },
+    {selector: '.about-list', offset: 215, callback: 'Materialize.showStaggeredList(".about-list")' },
+    {selector: '#image-test', offset: 215, callback: 'Materialize.fadeInImage("#image-test")' }
+  ];
+  Materialize.scrollFire(options);
+
+
 });
